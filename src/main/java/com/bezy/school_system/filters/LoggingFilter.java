1d2f7f9b4a5c62bd6bfee5ae7d1d6cf0b8215fea
Bezy427 <rusamubezel@gmail.com>
2025-11-04 22:59:14 +0200

    Rename the file

--- src/main/java/com/bezy/school_system/filters/LoggingFilter.java
+++ src/main/java/com/bezy/school_system/filters/LoggingFilter.java
@@ -12,9 +12,7 @@ import java.io.IOException;
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("Request: " + request.getRequestURI());
         filterChain.doFilter(request, response);
         System.out.println("Response: " + response.getStatus());
