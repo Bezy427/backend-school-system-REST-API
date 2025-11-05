a9dfafa369098d0763593b7387eec1bf72b7391e
Bezy427 <rusamubezel@gmail.com>
2025-11-05 15:52:12 +0200

    Modified requests

--- src/main/java/com/bezy/school_system/dtos/RegisterStudentRequest.java
+++ src/main/java/com/bezy/school_system/dtos/RegisterStudentRequest.java
@@ -1,6 +1,5 @@
 package com.bezy.school_system.dtos;
 
-import com.bezy.school_system.entities.Role;
 import com.bezy.school_system.entities.User;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
@@ -10,7 +9,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Builder
 @Data
@@ -57,122 +55,4 @@ public class RegisterStudentRequest {
     public boolean isPasswordsMatching() {
         return password != null && password.equals(confirmPassword);
     }
-
-    public RegisterStudentRequest(User user_id, String parentContact, String gender, LocalDate dateOfBirth,
-                                  String grade, String username, String firstName, String lastName,
-                                  String email, String registrationNumber, String password, Role role,
-                                  String confirmPassword) {
-        this.user_id = user_id;
-        this.parentContact = parentContact;
-        this.gender = gender;
-        this.dateOfBirth = dateOfBirth;
-        this.grade = grade;
-        this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.registrationNumber = registrationNumber;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-    }
-
-    public RegisterStudentRequest() {
-    }
-
-    public User getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(User user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getParentContact() {
-        return parentContact;
-    }
-
-    public void setParentContact(String parentContact) {
-        this.parentContact = parentContact;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
 }
